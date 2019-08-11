7fb57bf8da3447872a1a604480055e8c5d659e3d
emsonn <eabrahamson@knights.ucf.edu>
2019-08-11 03:31:52 -0400

    Fixed window to open in top-right of screen

--- keyboardvisualizer.py
+++ keyboardvisualizer.py
@@ -3,20 +3,25 @@ from tkinter import PhotoImage, Label
 
 HEIGHT = 210
 WIDTH = 510
+DOWN = 0
 
-# ws = root.winfo_screenwidth()
-# hs = root.winfo_screenheight()
-
+# Window initialization
 root = tk.Tk()
 
+# Window placement
+top_right = root.winfo_screenwidth()
+root.geometry("+{}+{}".format(top_right, DOWN))
+
+# Window sizing
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-# frame = tk.Frame(root, )
+# Image loading + adding
 filename = PhotoImage(file = "/home/eddie/Downloads/minidox.png")
 background_picture = Label(root, image=filename)
 background_picture.place(x=0, y=0, relwidth=1, relheight=1)
 
 canvas.pack()
+
 
 root.mainloop()
